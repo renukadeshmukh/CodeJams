14f377c5ac0a72233fdfadfa95911c5897565b90
renukadeshmukh <renud1988@gmail.com>
2015-02-27 20:38:43 -0500

    Added Codejam Filling Jars
    Former-commit-id: feca7ae342a7a87f44d57835b9ed2e63e735a17d

---
+++ FillingJars/FillingJars/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FillingJars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] inputs = input.Split(' ');
+            int n = Int32.Parse(inputs[0]);
+            int m = Int32.Parse(inputs[1]);
+
+            Double sum = 0;
+            for (int i = 0; i < m; i++)
+            {
+                string line = Console.ReadLine();
+
+                string[] lines = line.Split(' ');
+                long a = Int32.Parse(lines[0]);
+                long b = Int32.Parse(lines[1]);
+                long k = Int32.Parse(lines[2]);
+
+                sum = sum + ((b - a) + 1) * k;
+            }
+
+            double result = Math.Floor(sum / n);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
