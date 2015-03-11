1e4cbc482ddc23815dd178a5b4c78a8f2938e0d5
renukadeshmukh <renud1988@gmail.com>
2015-03-11 16:59:41 -0400

    Worst Permutation - Hacker Rank
    Former-commit-id: d98bdb739c235b7f14f4ebf12a799ee260bf2bcb

---
+++ WorstPermutation/WorstPermutation/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorstPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] ipArr = input.Split(' ');
+
+            long n = Int64.Parse( ipArr[0]);
+            long k = Int64.Parse(ipArr[1]);
+
+            string nums = Console.ReadLine();
+            string[] numArr = nums.Split(' ');
+
+            long[] arr = new long[n];
+
+            for (long i = 0; i < numArr.Length; i++)
+            {
+                arr[i] = Int64.Parse(numArr[i]);
+            }
+
+            long largestNum = numArr.Length;
+            //long indx = 0;
+                
+            for (long i = 0; i < arr.Length; i++)
+            {
+                long j = i;
+                while (arr[j] != largestNum && j<arr.Length)
+                {
+                    j++;
+                }
+                largestNum--;
+                if (arr[i] != arr[j])
+                {
+                    long temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    k--;
+                    if (k == 0)
+                        break;
+                }
+            }
+            string output = string.Empty;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                output = output + arr[i] + " ";
+            }
+            Console.WriteLine(output.Trim());
+            Console.ReadKey();
+        }
+    }
+}
